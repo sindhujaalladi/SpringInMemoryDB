20c217f004f2baf5ad1036071ea3ee4f8ff788f5
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-10-21 22:30:20 -0500

    SPRING h2 database files

--- src/main/java/com/inmemory/springinmemorydb/SpringInMemoryDbApplication.java
+++ src/main/java/com/inmemory/springinmemorydb/SpringInMemoryDbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringInMemoryDbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringInMemoryDbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInMemoryDbApplication.class, args);
+    }
 
 }
